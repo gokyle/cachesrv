f43768f4b31d715a69faa8e437e62e30b1200d25
Kyle Isom <coder@kyleisom.net>
2012-11-27 22:26:38 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,67 @@
+srvwd - serve working directory
+-------------------------------
+cachesrv is a web server that serves the current working directory using a
+file cache to speed up commonly-accessed files.
+
+Dependencies
+------------
+None.
+
+Compatibility
+-------------
+`cachesrv` has been tested on the following operating systems:
+* OpenBSD (5.2)
+* OS X (10.8)
+
+
+Installation
+------------
+`go install` will install the binary to the `${GOROOT}/bin` directory.
+
+
+Usage
+-----
+`cachesrv [options] [dir]`
+
+Valid options:
+```
+        -c certfile     specify the TLS certificate
+        -e seconds      seconds to expire cache items after; 0 to never expire
+                        due to time in cache
+        -g seconds      seconds to delay between checks for expired items in the
+                        cache.
+        -k key          specify the TLS key
+        -n items        maximum number of files to store in cache
+        -p              the port to listen on
+        -r              the server should chroot to the target directory
+        -u user         the server should drop privileges to this user
+        -v              display the version and exit
+```
+
+Specifying a key and certificate will cause the server to listen in TLS
+mode. 
+
+Use `^C` to halt the server.
+
+
+Why require sudo for chroot?
+----------------------------
+From [chroot(2)](http://www.openbsd.org/cgi-bin/man.cgi?query=chroot&apropos=0&sektion=2&manpath=OpenBSD+Current&arch=i386&format=ascii):
+
+     This call is restricted to the super-user.
+
+The server uses root privileges to chroot to the target directory, then
+immediately drops privileges.
+
+
+Known bugs / caveats
+--------------------
+setgrp isn't implemented here, as no good solution exists in Go.
+
+
+History
+-------
+This is version 3.0.0 of the `srvwd` file server. The original version
+(the 1.x series) was written in C; a subsequent rewrite in Go (the 2.x)
+version used only the standard library to serve files. This version,
+the 3.x series, uses a file cache to speed up commonly accessed files.
---
+++ cachesrv.go
@@ -0,0 +1,165 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/gokyle/filecache"
+	"log"
+	"net/http"
+	"os"
+	"os/user"
+	"path/filepath"
+	"regexp"
+	"strconv"
+	"syscall"
+	"time"
+)
+
+const VERSION = "2.0.0"
+
+var (
+	cache        *filecache.FileCache
+	path_regexp  = regexp.MustCompile("^/(.*)$")
+	space_regexp = regexp.MustCompile(" ")
+	srv_bin      string
+)
+
+func init() {
+	cache = filecache.NewCache()
+	filecache.DefaultExpireItem = 0
+	cache.Start()
+}
+
+func Path(r *http.Request) (path string) {
+	path = path_regexp.ReplaceAllString(r.URL.Path, "./$1")
+	if len(path) == 0 {
+		path = "."
+	}
+	return path
+}
+
+func Dispatch(w http.ResponseWriter, r *http.Request) {
+	if r.URL.RawQuery == "cachestats" {
+		go displayCacheStats()
+	}
+	fi, err := os.Stat(Path(r))
+	if err != nil {
+		http.ServeFile(w, r, Path(r))
+	} else if fi.IsDir() {
+		http.ServeFile(w, r, Path(r))
+	} else if fi.Size() > cache.MaxSize {
+		http.ServeFile(w, r, Path(r))
+	} else {
+	        cache.HttpWriteFile(w, r)
+	}
+	return
+}
+
+func displayCacheStats() {
+	fmt.Printf("-----[ cache stats: %s ]-----\n",
+		time.Now().Format("2006-01-02 15:04:05"))
+	fmt.Println("files cached: ", cache.Size())
+	fmt.Printf("cache size: %d bytes\n", cache.FileSize())
+	cachedFiles := cache.StoredFiles()
+	fmt.Println("[ cached files ]")
+	for _, name := range cachedFiles {
+		fmt.Printf("\t* %s\n", name)
+	}
+	fmt.Printf("\n\n")
+}
+
+func main() {
+	var (
+		srv_port int
+		srv_ssl  bool
+		srv_wd   = "."
+	)
+
+	srv_bin = filepath.Base(os.Args[0])
+	empty := func(s string) bool {
+		return len(s) == 0
+	}
+
+	fCert := flag.String("c", "", "TLS certificate file")
+	fExpire := flag.Int("e", filecache.DefaultExpireItem,
+		"maximum number of seconds between accesses a file can stay "+
+			"in the cache")
+	fGarbage := flag.Int("g", filecache.DefaultEvery,
+		"scan the cache for expired items every <n> seconds")
+	fKey := flag.String("k", "", "TLS key file")
+	fItems := flag.Int("n", filecache.DefaultMaxItems, "max number of "+
+		"files to store in the cache")
+	fPort := flag.Int("p", 8080, "port to listen on")
+	fChroot := flag.Bool("r", false, "chroot to the working directory")
+	fSize := flag.Int64("s", filecache.DefaultMaxSize,
+		"max file size to cache")
+	fUser := flag.String("u", "", "user to run as")
+	fVersion := flag.Bool("v", false, "print version information")
+	flag.Parse()
+
+	if *fVersion {
+		version()
+	}
+
+	if flag.NArg() > 0 {
+		srv_wd = flag.Arg(0)
+	}
+
+	if *fChroot {
+		srv_wd = chroot(srv_wd)
+	}
+
+	if !empty(*fUser) {
+		setuid(*fUser)
+	}
+
+	if !empty(*fCert) && !empty(*fKey) {
+		srv_ssl = true
+	}
+
+	cache.MaxItems = *fItems
+	cache.ExpireItem = *fExpire
+	cache.Every = *fGarbage
+	cache.MaxSize = *fSize
+	srv_wd, err := filepath.Abs(srv_wd)
+	checkFatal(err)
+	err = os.Chdir(srv_wd)
+	checkFatal(err)
+	srv_port = *fPort
+	srv_addr := fmt.Sprintf(":%d", srv_port)
+	fmt.Printf("serving %s on %s\n", srv_wd, srv_addr)
+	http.HandleFunc("/", Dispatch)
+	cache.Start()
+	if srv_ssl {
+		log.Fatal(http.ListenAndServeTLS(srv_addr, *fCert, *fKey, nil))
+	} else {
+		log.Fatal(http.ListenAndServe(srv_addr, nil))
+	}
+}
+
+func checkFatal(err error) {
+	if err != nil {
+		fmt.Printf("[!] %s: %s\n", srv_bin, err.Error())
+		os.Exit(1)
+	}
+}
+
+func setuid(username string) {
+	usr, err := user.Lookup(username)
+	checkFatal(err)
+	uid, err := strconv.Atoi(usr.Uid)
+	checkFatal(err)
+	err = syscall.Setreuid(uid, uid)
+	checkFatal(err)
+}
+
+func chroot(path string) string {
+	err := syscall.Chroot(path)
+	checkFatal(err)
+	return "/"
+}
+
+func version() {
+	fmt.Printf("%s version %s\n", srv_bin, VERSION)
+	os.Exit(0)
+}
